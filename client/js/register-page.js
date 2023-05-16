55fddfa95e77fd4dee68260ad658fee590b9708c
Stanislaw <stas.yarets234@gmail.com>
2023-05-16 17:31:56 +0300

    Add register page

---
+++ client/js/register-page.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('.registerForm');
+const formGroups = document.querySelectorAll('.registerForm .form-group')
+form.addEventListener('submit', (e)=> {
+    if(!form.checkValidity()) {
+        e.preventDefault();
+    }
+    // form.classList.add('was-validated');
+    formGroups.forEach((item)=> {
+       if(item.classList.contains('repeatPassword')) {
+           if(form.elements.password.value !== form.elements.repeatPassword.value) {
+               form.elements.repeatPassword.classList.add('is-invalid');
+               e.preventDefault();
+           } else {
+               form.elements.repeatPassword.classList.remove('is-invalid');
+               item.classList.add('was-validated');
+           }
+       } else {
+           item.classList.add('was-validated');
+       }
+    });
+
+});

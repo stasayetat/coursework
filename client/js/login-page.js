a94e5db34a308567e7f3fb1f875dd92f85aff7df
Stanislaw <stas.yarets234@gmail.com>
2023-05-16 17:37:27 +0300

    Add login page

---
+++ client/js/login-page.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('.loginForm');
+form.addEventListener('submit', (e)=> {
+    if(!form.checkValidity()) {
+        e.preventDefault();
+    }
+    form.classList.add('was-validated');
+});

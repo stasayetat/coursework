4834087d3c4e647dd5d2305137ca4d347c9dc6ea
Stanislaw <stas.yarets234@gmail.com>
2023-05-18 20:27:35 +0300

    add header.js, start write js-code

---
+++ client/js/header.js
@@ -0,0 +1,25 @@
+import axios from 'https://cdn.skypack.dev/axios';
+
+let searchButton = document.querySelector('#searchButton');
+let searchInput = document.querySelector('#searchInput');
+searchButton.addEventListener('click', async ()=> {
+    let searchValue = searchInput.value;
+    await axios.get(`/search/name=${searchValue}`);
+});
+
+let userIcon = document.querySelector('#userIcon');
+userIcon.addEventListener('click', async ()=> {
+    // await axios.get('/user/main/');
+});
+
+let savedItems = document.querySelector('#savedItems');
+savedItems.addEventListener('click', async ()=> {
+    // await axios.get('/user/saved/');
+});
+
+let cartItem = document.querySelector('#savedItems');
+cartItem.addEventListener('click', async ()=> {
+    // await axios.get('/cart/');
+});
+
+
--- client/js/order-page.js
+++ client/js/order-page.js
@@ -1,8 +1,13 @@
 // let incButton = document.querySelector('.incButton');
+import axios from 'https://cdn.skypack.dev/axios';
+
 let decButts = document.getElementsByClassName('decButton');
 let incButts = document.getElementsByClassName('incButton');
+let closeButts = document.getElementsByClassName('close-btn');
+let boughtItems = document.querySelector('#boughtItems');
 let realIncButts = Array.from(incButts);
 let realDecButts = Array.from(decButts);
+let realCloseButts = Array.from(closeButts);
 realIncButts.forEach((item)=> {
     item.addEventListener('click', ()=> {
         let quantityValue = item.previousElementSibling;
@@ -21,10 +26,39 @@ realDecButts.forEach((item)=> {
     })
 });
 
+realCloseButts.forEach((item)=> {
+    item.addEventListener('click', ()=> {
+        let thisItem = item.closest('.bt-item');
+        thisItem.remove();
+        if(boughtItems.childElementCount < 2) {
+            const spanElement = document.createElement('span');
+            spanElement.classList.add('fw-bold', 'fs-4');
+            spanElement.textContent = 'Корзина порожня :(';
+            boughtItems.appendChild(spanElement);
+        }
+    });
+})
+
+
+
 const form = document.querySelector('.orderForm');
-form.addEventListener('submit', (e)=> {
-    if(!form.checkValidity()) {
+form.addEventListener('submit', async (e)=> {
+    if (!form.checkValidity() || !boughtItems.children[1].classList.contains('bt-item')) {
         e.preventDefault();
+        form.classList.add('was-validated');
+    } else {
+        const products = [];
+        const btItemElements = document.getElementsByClassName('bt-item');
+        for(let el of btItemElements) {
+            const product = {
+                itemName: el.querySelector('.itemName').innerText,
+                itemAmount: el.querySelector('.itemAmount').value
+            }
+            products.push(product)
+        }
+        const formData = new FormData(form);
+        formData.append('products', JSON.stringify(products));
+        await axios.post('/order', formData);
     }
-    form.classList.add('was-validated');
+
 });
